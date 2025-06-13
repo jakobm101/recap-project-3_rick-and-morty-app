cce522cbc05a5f8dd6a0dbca74d0a32d314221a0
Joseph <joseph.urbina61@hotmail.com>
2025-06-13 15:48:11 +0200

    Rick-and-morty API Fetch complete

--- components/Fetch/fetch.js
+++ components/Fetch/fetch.js
@@ -1,1 +1,10 @@
-// here be dragons
+export const fetchData = async (newURL = URL) => {
+  const URL = `https://rickandmortyapi.com/api/character`;
+  try {
+    const response = await fetch(newURL);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
