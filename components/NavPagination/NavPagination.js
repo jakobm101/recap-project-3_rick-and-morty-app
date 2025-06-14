afd43a074edba90b225dc7fa032cf647e43a9683
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-14 15:24:03 +0200

    functioning

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -4,13 +4,19 @@ import createCards from "../CharacterCard/CharacterCard.js";
 export const pageTurn = async (next) => {
   const main = document.querySelector("main");
   let fetching;
+  try {
     fetching = await fetchData(next);
-  if (typeof fetching === "object") {
+  } catch (error) {
+    log.error(error);
+  }
+  // fallback if the new page does not exist
+  if (typeof fetching.results !== "object") {
+    fetching = await fetchData();
+    console.log('yo');
+    
+  } else {
     main.innerHTML = "";
     main.append(createCards(fetching.results));
-  } else {
-    alert("no more pages");
+    return fetching;
   }
-  return fetching
 };
-
