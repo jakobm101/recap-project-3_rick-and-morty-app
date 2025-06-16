d6b952d15c7ecfe8109b0a3882b13434070cabb6
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-16 11:57:45 +0200

    error message from fetching dealt with

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -7,8 +7,9 @@ export const pageRender = async (data = null, pageToRender = URL) => {
   let currentPage = pageToRender?.split("=")[1] || "1";
   // the fetch is actually coming from the module not from index
   // but now i need to add data
+  
   let fetching;
-  if (!data) fetching = await fetchData(pageToRender);
+  if (!data && pageToRender) fetching = await fetchData(pageToRender);
 
   // fallback if the new page does not exist
   if (!data && typeof fetching !== "object") fetching = await fetchData();
