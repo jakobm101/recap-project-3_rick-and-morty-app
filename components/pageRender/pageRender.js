afb9e3d6fdf349b67201ef142ef334011475396f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-16 23:14:39 +0200

    renaming variables and error logging

--- components/pageRender/pageRender.js
+++ components/pageRender/pageRender.js
@@ -5,14 +5,15 @@ const main = document.querySelector("main");
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 
-export const pageRender = async (currentData, isSearch, prevOrNext) => {
+export const pageRender = async (importedData, isSearch, prevOrNext) => {
   //check if next or previous page should be rendered
   let pageToRender = startPage; // initial case
-  if (prevOrNext === "next") pageToRender = currentData.info.next;
-  if (prevOrNext === "prev") pageToRender = currentData.info.prev;
+  if (prevOrNext === "next") pageToRender = importedData.info.next;
+  if (prevOrNext === "prev") pageToRender = importedData.info.prev;
+  if (prevOrNext !== "next" && prevOrNext !== "prev" && prevOrNext) log.error("wrong input");
 
   // fetch data if not searching
-  let data = isSearch ? currentData : await fetchData(pageToRender);
+  let data = isSearch ? importedData : await fetchData(pageToRender);
 
   //create cards and update related components
   main.innerHTML = "";
@@ -26,6 +27,6 @@ export const pageRender = async (currentData, isSearch, prevOrNext) => {
   const urlPage = new URL(pageToRender).searchParams?.get("page");
   pagination.textContent = `${urlPage ?? 1}／${data.info.pages}`;
 
-  // RETURN
+  // RETURN state
   return data;
 };
